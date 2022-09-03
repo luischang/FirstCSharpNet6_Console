57b2fc6ecb7f0042fc676096c730c38d18c45dcb
Luis Chang <luis.chang@iugo.com.uy>
2022-09-03 08:50:58 -0500

    Agregar archivos de proyecto.

---
+++ FirstCSharpNet6Console/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpNet6Console
+{
+    public class Persona
+    {
+        public int dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public string telefono { get; set; }
+        public bool estado { get; set; }
+
+        public Persona()
+        {
+
+        }
+        
+        public Persona(int dni, string nombreCompleto, string telefono, bool estado)
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.telefono = telefono;
+            this.estado = estado;
+        }
+
+    }
+}
---
+++ FirstCSharpNet6Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string curso = "Desarrollo de aplicaciones web 2022-01";
+int edad = 20;
+double salario = 1200;
+
+var alumno = "Luis Chang";
+var peso = 75;
+
+
+
+
+
